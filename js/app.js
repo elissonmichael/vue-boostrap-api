ad04cec38368b7d61fb441b31643634a3251cccb
Élisson Michael <elissonmichael@gmail.com>
2019-10-29 14:44:28 -0300

    Add Edit Form

--- js/app.js
+++ js/app.js
@@ -36,19 +36,35 @@ const app = new Vue({
     template: `
     <tbody>
       <tr v-for="character, i in characters">
-        <td>{{character.id}}</td>
-        <td>{{character.name}}</td>
-        <td>{{character.strength}}</td>
-        <td>{{character.dexterity}}</td>
-        <td>{{character.constitution}}</td>
-        <td>{{character.intelligence}}</td>
-        <td>{{character.wisdom}}</td>
-        <td>{{character.charisma}}</td>
-        <td>
-          <div class="btn-group btn-group-toggle" data-toggle="buttons">
-            <button class="btn btn-outline-danger" v-on:click="deleteCharacter(character.id, i)">Delete</button>
-          </div>
-        </td>
+        <template v-if="editCharacter === character.id">
+          <td colspan="2"><input class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.name" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.strength" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.dexterity" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.constitution" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.intelligence" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.wisdom" /></td>
+          <td><input type='number' class="form-control" v-on:keyup.13="updateCharacter(character)" v-model="character.charisma" /></td>
+          <td>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              <button class="btn btn-outline-info" v-on:click="updateCharacter(character)">Save</button>
+            </div>
+          </td>
+        </template>
+        <template v-else>
+          <td colspan="2">{{character.name}}</td>
+          <td>{{character.strength}}</td>
+          <td>{{character.dexterity}}</td>
+          <td>{{character.constitution}}</td>
+          <td>{{character.intelligence}}</td>
+          <td>{{character.wisdom}}</td>
+          <td>{{character.charisma}}</td>
+          <td>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              <button class="btn btn-outline-primary" v-on:click="editCharacter = character.id">Edit</button>
+              <button class="btn btn-outline-danger" v-on:click="deleteCharacter(character.id, i)">Delete</button>
+            </div>
+          </td>
+        </template>
       </tr>
     </tbody>
     `,
